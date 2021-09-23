0ccf0494c5964a55a05cfc0e01e01b1c6894ada2
Brett Feddersen <bfeddrsn@illinois.edu>
2021-09-23 17:08:16 -0500

    Adding splash screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
 
 
 #include <QApplication>
+#include <QSplashScreen>
 
 #include "MainWindow.hpp"
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+
+    QPixmap* pPixmap = new QPixmap(":/ripe.illinois.edu/splash.png");
+    QSplashScreen* pSplash = new QSplashScreen(*pPixmap);
+    pSplash->show();
+    pSplash->showMessage("Initializing sensors...", Qt::AlignHCenter | Qt::AlignBottom);
+
     cMainWindow mainWin;
     mainWin.show();
+
+    pSplash->finish(&mainWin);
+
+    delete pSplash; 
+    pSplash = nullptr;
+    
+    delete pPixmap;
+    pPixmap = nullptr;
 
 	return app.exec();
 }
