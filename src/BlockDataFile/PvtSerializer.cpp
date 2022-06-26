5d730cb7e64d8f7632d5ca9fa5a774eee478d865
Brett Feddersen <bfeddrsn@illinois.edu>
2022-06-26 10:02:03 -0500

    Initial commit

---
+++ src/BlockDataFile/PvtSerializer.cpp
@@ -0,0 +1,155 @@
+
+#include "PvtSerializer.hpp"
+#include "PvtDataIdentifiers.hpp"
+#include "BlockDataFile.hpp"
+
+
+#include <cassert>
+
+using namespace pvt;
+
+cPvtSerializer::cPvtSerializer()
+:
+    cBlockSerializer()
+{
+    setVersion(1, 0);
+}
+
+cPvtSerializer::cPvtSerializer(std::size_t n, cBlockDataFileWriter* pDataFile)
+:
+    cBlockSerializer(n, pDataFile)
+{
+    setVersion(1, 0);
+}
+
+cBlockID& cPvtSerializer::blockID()
+{
+    return mBlockID;
+}
+
+void cPvtSerializer::write(ePOSTION_UNITS unit)
+{
+    assert(mpDataFile);
+
+    mBlockID.dataID(DataID::POSITION_UNIT);
+
+    mDataBuffer.clear();
+    mDataBuffer << static_cast<uint8_t>(unit);
+
+    mpDataFile->writeBlock(mBlockID, mDataBuffer.data(), mDataBuffer.size());
+}
+
+void cPvtSerializer::write(eVELOCITY_UNITS unit)
+{
+    assert(mpDataFile);
+
+    mBlockID.dataID(DataID::VELOCITY_UNIT);
+
+    mDataBuffer.clear();
+    mDataBuffer << static_cast<uint8_t>(unit);
+
+    mpDataFile->writeBlock(mBlockID, mDataBuffer.data(), mDataBuffer.size());
+}
+
+void cPvtSerializer::write(eTIME_UNITS unit)
+{
+    assert(mpDataFile);
+
+    mBlockID.dataID(DataID::TIME_UNIT);
+
+    mDataBuffer.clear();
+    mDataBuffer << static_cast<uint8_t>(unit);
+
+    mpDataFile->writeBlock(mBlockID, mDataBuffer.data(), mDataBuffer.size());
+}
+
+void cPvtSerializer::write_position(double x)
+{
+    assert(mpDataFile);
+
+	mBlockID.dataID(DataID::POSITION_1D);
+
+    mDataBuffer.clear();
+    mDataBuffer << x;
+
+    mpDataFile->writeBlock(mBlockID, mDataBuffer.data(), mDataBuffer.size());
+}
+
+void cPvtSerializer::write_position(double x, double y)
+{
+    assert(mpDataFile);
+
+    mBlockID.dataID(DataID::POSITION_2D);
+
+    mDataBuffer.clear();
+    mDataBuffer << x;
+    mDataBuffer << y;
+
+    mpDataFile->writeBlock(mBlockID, mDataBuffer.data(), mDataBuffer.size());
+}
+
+void cPvtSerializer::write_position(double x, double y, double z)
+{
+    assert(mpDataFile);
+
+    mBlockID.dataID(DataID::POSITION_3D);
+
+    mDataBuffer.clear();
+    mDataBuffer << x;
+    mDataBuffer << y;
+    mDataBuffer << z;
+
+    mpDataFile->writeBlock(mBlockID, mDataBuffer.data(), mDataBuffer.size());
+}
+
+void cPvtSerializer::write_velocity(double x)
+{
+    assert(mpDataFile);
+
+    mBlockID.dataID(DataID::VELOCITY_1D);
+
+    mDataBuffer.clear();
+    mDataBuffer << x;
+
+    mpDataFile->writeBlock(mBlockID, mDataBuffer.data(), mDataBuffer.size());
+}
+
+void cPvtSerializer::write_velocity(double x, double y)
+{
+    assert(mpDataFile);
+
+    mBlockID.dataID(DataID::VELOCITY_2D);
+
+    mDataBuffer.clear();
+    mDataBuffer << x;
+    mDataBuffer << y;
+
+    mpDataFile->writeBlock(mBlockID, mDataBuffer.data(), mDataBuffer.size());
+}
+
+void cPvtSerializer::write_velocity(double x, double y, double z)
+{
+    assert(mpDataFile);
+
+    mBlockID.dataID(DataID::VELOCITY_3D);
+
+    mDataBuffer.clear();
+    mDataBuffer << x;
+    mDataBuffer << y;
+    mDataBuffer << z;
+
+    mpDataFile->writeBlock(mBlockID, mDataBuffer.data(), mDataBuffer.size());
+}
+
+void cPvtSerializer::write_timestamp(std::uint64_t t)
+{
+    assert(mpDataFile);
+
+    mBlockID.dataID(DataID::TIMESTAMP);
+
+    mDataBuffer.clear();
+    mDataBuffer << t;
+
+    mpDataFile->writeBlock(mBlockID, mDataBuffer.data(), mDataBuffer.size());
+}
+
