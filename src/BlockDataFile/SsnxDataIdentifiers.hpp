c997c2a0100c331df390f09f85e11aed6d0dd0c9
Brett Feddersen <bfeddrsn@illinois.edu>
2022-07-10 11:12:43 -0500

    Added warning comment

--- src/BlockDataFile/SsnxDataIdentifiers.hpp
+++ src/BlockDataFile/SsnxDataIdentifiers.hpp
@@ -3,6 +3,11 @@
 
 namespace ssnx
 {
+    /**********************************************************
+       WARNING: DO NOT CHANGE THE ORDER OF THESE ENUMS!!!!
+
+       Only add new one to the end of the list.
+    **********************************************************/
     enum class DataID : uint16_t
     {
         PVT_CARTESIAN,
