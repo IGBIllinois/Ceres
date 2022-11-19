791f062eab6c45bbcf5dea151f62d82aba7eb613
Brett Feddersen <bfeddrsn@illinois.edu>
2022-11-19 12:57:51 -0600

    Initial commit

---
+++ src/Sensors/Lidar/OusterAsyncCommands.cpp
@@ -0,0 +1,279 @@
+
+#include "OusterAsyncCommands.hpp"
+
+#include "OusterModel_net.hpp"
+#include "../../Utilities/Constants.hpp"
+
+#include <ouster/ouster_utils.h>
+#include <optional>
+#include <string>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// The OUSTER Async Command classes
+///////////////////////////////////////////////////////////////////////////////
+
+cOusterAsyncCmd::cOusterAsyncCmd(cOusterModel_net* pModel) : mpModel(pModel) {};
+
+cOusterCmdStream_Qt& cOusterAsyncCmd::cmdStream()
+{
+    return mpModel->mCmdStream;
+}
+
+void cOusterAsyncCmd::setStatus(const sensor::eStatus status)
+{
+    mpModel->setStatus(status);
+}
+
+void cOusterAsyncCmd::statusMessage(QString& msg)
+{
+    mpModel->emitStatusMessage(msg);
+}
+
+
+/// Reinitialization
+cOusterAsyncCmd_Reinitialize::cOusterAsyncCmd_Reinitialize(cOusterModel_net* pModel)
+    : cOusterAsyncCmd(pModel)
+{}
+
+bool cOusterAsyncCmd_Reinitialize::postCmd()
+{
+    if (!cmdStream().postReinitialize())
+    {
+        setStatus(sensor::eStatus::FAILED);
+        QString msg = "OUSTER failed to reinitialize.";
+        statusMessage(msg);
+        return false;
+    }
+
+    return true;
+}
+
+bool cOusterAsyncCmd_Reinitialize::complete()
+{
+    return cmdStream().reinitializeComplete();
+}
+
+
+// Lidar Mode
+cOusterAsyncCmd_SetLidarMode::cOusterAsyncCmd_SetLidarMode(cOusterModel_net* pModel, ouster::eLIDAR_MODE mode)
+  : cOusterAsyncCmd(pModel), mMode(mode) 
+{}
+
+bool cOusterAsyncCmd_SetLidarMode::postCmd()
+{
+    setStatus(sensor::eStatus::REINITIALIZING);
+
+    QString msg = "Setting lidar mode to ";
+    msg += QString::fromStdString(to_string(mMode));
+    statusMessage(msg);
+
+    if (!cmdStream().postLidarMode(mMode))
+    {
+        setStatus(sensor::eStatus::FAILED);
+
+        QString msg = "Failed to set the lidar mode to ";
+        msg += QString::fromStdString(to_string(mMode));
+        statusMessage(msg);
+        return false;
+    }
+
+    return true;
+}
+
+bool cOusterAsyncCmd_SetLidarMode::complete()
+{
+    try
+    {
+        return cmdStream().postConfigParamComplete();
+    }
+    catch (const std::exception& e)
+    {
+        qCritical() << "Exception Set Lidar Mode:" << e.what();
+        setStatus(sensor::eStatus::FAILED);
+    }
+
+    return true;
+}
+
+
+cOusterAsyncCmd_GetLidarMode::cOusterAsyncCmd_GetLidarMode(cOusterModel_net* pModel, ouster::eLIDAR_MODE mode)
+    : cOusterAsyncCmd(pModel), mMode(mode)
+{}
+
+bool cOusterAsyncCmd_GetLidarMode::postCmd()
+{
+    if (!cmdStream().queryLidarMode(true))
+    {
+        setStatus(sensor::eStatus::FAILED);
+        return false;
+    }
+
+    return true;
+}
+
+bool cOusterAsyncCmd_GetLidarMode::complete()
+{
+    try
+    {
+        ouster::eLIDAR_MODE mode;
+        if (!cmdStream().lidarModeComplete(mode))
+        {
+            return false;
+        }
+
+        bool result = (mode == mMode);
+
+        setStatus(result ? sensor::eStatus::RUNNING : sensor::eStatus::FAILED);
+
+        QString msg = "Lidar mode set to ";
+        msg += QString::fromStdString(to_string(mode));
+        statusMessage(msg);
+
+        mpModel->setLidarMode(mode);
+    }
+    catch (const std::exception& e)
+    {
+        qCritical() << "Exception Get Lidar Mode:" << e.what();
+        setStatus(sensor::eStatus::FAILED);
+    }
+
+    return true;
+}
+
+/// Azimuth Window 
+
+cOusterAsyncCmd_SetAzimuthWindow::cOusterAsyncCmd_SetAzimuthWindow(cOusterModel_net* pModel, double min_deg, double max_deg)
+    : cOusterAsyncCmd(pModel), mMin_deg(min_deg), mMax_deg(max_deg) 
+{}
+
+bool cOusterAsyncCmd_SetAzimuthWindow::postCmd()
+{
+    setStatus(sensor::eStatus::REINITIALIZING);
+
+    QString msg = "Setting azimuth window set to (";
+    msg += QString::number(mMin_deg);
+    msg += ", ";
+    msg += QString::number(mMax_deg);
+    msg += ")";
+
+    statusMessage(msg);
+
+    if (!cmdStream().postAzimuthWindow(mMin_deg, mMax_deg))
+    {
+        setStatus(sensor::eStatus::FAILED);
+        qCritical() << "Set azimuth window failed!";
+        return false;
+    }
+
+    return true;
+}
+
+bool cOusterAsyncCmd_SetAzimuthWindow::complete()
+{
+    try
+    {
+        return cmdStream().postConfigParamComplete();
+    }
+    catch (const std::exception& e)
+    {
+        qCritical() << "Exception Set Azimuth Window:" << e.what();
+        setStatus(sensor::eStatus::FAILED);
+    }
+
+    return true;
+}
+
+
+cOusterAsyncCmd_GetAzimuthWindow::cOusterAsyncCmd_GetAzimuthWindow(cOusterModel_net* pModel, double min_deg, double max_deg)
+    : cOusterAsyncCmd(pModel), mMin_deg(min_deg), mMax_deg(max_deg)
+{}
+
+bool cOusterAsyncCmd_GetAzimuthWindow::postCmd()
+{
+    if (!cmdStream().queryAzimuthWindow(true))
+    {
+        setStatus(sensor::eStatus::FAILED);
+        qCritical() << "Query azimuth window failed!";
+        return false;
+    }
+
+    return true;
+}
+
+bool cOusterAsyncCmd_GetAzimuthWindow::complete()
+{
+    try
+    {
+        ouster::azimuth_range_t range;
+        if (!cmdStream().azimuthWindowComplete(range.min_deg, range.max_deg))
+        {
+            return false;
+        }
+
+        bool result = (range.min_deg == mMin_deg) && (range.max_deg == mMax_deg);
+
+        setStatus(result ? sensor::eStatus::RUNNING : sensor::eStatus::FAILED);
+
+        QString msg = "Azimuth window is set to (";
+        msg += QString::number(range.min_deg);
+        msg += ", ";
+        msg += QString::number(range.max_deg);
+        msg += ")";
+
+        statusMessage(msg);
+
+        mpModel->setAzimuthWindow(range);
+    }
+    catch (const std::exception& e)
+    {
+        qCritical() << "Exception Get Azimuth Window:" << e.what();
+        setStatus(sensor::eStatus::FAILED);
+    }
+
+    return true;
+}
+
+
+/// Lidar Data Format
+
+cOusterAsyncCmd_GetLidarDataFormat::cOusterAsyncCmd_GetLidarDataFormat(cOusterModel_net* pModel)
+    : cOusterAsyncCmd(pModel)
+{}
+
+bool cOusterAsyncCmd_GetLidarDataFormat::postCmd()
+{
+    if (!cmdStream().queryLidarDataFormat())
+    {
+        setStatus(sensor::eStatus::FAILED);
+        qCritical() << "Query lidar data format failed!";
+        return false;
+    }
+
+    return true;
+}
+
+bool cOusterAsyncCmd_GetLidarDataFormat::complete()
+{
+    try
+    {
+        auto format = cmdStream().lidarDataFormatComplete();
+        if (!format.has_value())
+        {
+            return false;
+        }
+
+        mpModel->setLidarDataFormat(format.value());
+    }
+    catch (const std::exception& e)
+    {
+        qCritical() << "Exception Get Lidar Data Format:" << e.what();
+        setStatus(sensor::eStatus::FAILED);
+    }
+
+    return true;
+}
+
+
+
+
---
+++ src/Sensors/Lidar/OusterAsyncCommands.hpp
@@ -0,0 +1,118 @@
+
+#pragma once
+
+#include <QString>
+
+class cOusterModel_net;
+class cOusterCmdStream_Qt;
+
+namespace sensor
+{
+    enum class eStatus;
+}
+
+namespace ouster
+{
+    enum class eLIDAR_MODE : uint8_t;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// The OUSTER Async Command classes
+///////////////////////////////////////////////////////////////////////////////
+
+class cOusterAsyncCmd
+{
+public:
+    cOusterAsyncCmd(cOusterModel_net* pModel);
+    virtual ~cOusterAsyncCmd() = default;
+
+    virtual bool postCmd() = 0;
+    virtual bool complete() = 0;
+
+protected:
+    cOusterCmdStream_Qt& cmdStream();
+
+    void setStatus(const sensor::eStatus status);
+
+    void statusMessage(QString& msg);
+
+protected:
+    cOusterModel_net* mpModel;
+};
+
+// Reinitialization
+
+class cOusterAsyncCmd_Reinitialize : public cOusterAsyncCmd
+{
+public:
+    cOusterAsyncCmd_Reinitialize(cOusterModel_net* pModel);
+
+    bool postCmd() override;
+    bool complete() override;
+};
+
+// Lidar Modes 
+class cOusterAsyncCmd_SetLidarMode : public cOusterAsyncCmd
+{
+public:
+    cOusterAsyncCmd_SetLidarMode(cOusterModel_net* pModel,
+        ouster::eLIDAR_MODE mode);
+
+    bool postCmd() override;
+    bool complete() override;
+
+private:
+    ouster::eLIDAR_MODE mMode;
+};
+
+class cOusterAsyncCmd_GetLidarMode : public cOusterAsyncCmd
+{
+public:
+    cOusterAsyncCmd_GetLidarMode(cOusterModel_net* pModel, 
+        ouster::eLIDAR_MODE mode);
+
+    bool postCmd() override;
+    bool complete() override;
+
+private:
+    ouster::eLIDAR_MODE mMode;
+};
+
+// Azimuth Window 
+
+class cOusterAsyncCmd_SetAzimuthWindow : public cOusterAsyncCmd
+{
+public:
+    cOusterAsyncCmd_SetAzimuthWindow(cOusterModel_net* pModel, double min_deg, double max_deg);
+
+    bool postCmd() override;
+    bool complete() override;
+
+private:
+    double mMin_deg;
+    double mMax_deg;
+};
+
+class cOusterAsyncCmd_GetAzimuthWindow : public cOusterAsyncCmd
+{
+public:
+    cOusterAsyncCmd_GetAzimuthWindow(cOusterModel_net* pModel, double min_deg, double max_deg);
+
+    bool postCmd() override;
+    bool complete() override;
+
+private:
+    double mMin_deg;
+    double mMax_deg;
+};
+
+// LiDAR Data Format
+
+class cOusterAsyncCmd_GetLidarDataFormat : public cOusterAsyncCmd
+{
+public:
+    cOusterAsyncCmd_GetLidarDataFormat(cOusterModel_net* pModel);
+
+    bool postCmd() override;
+    bool complete() override;
+};
