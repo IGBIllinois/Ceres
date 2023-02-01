e42203018488d41c35b3681f9d187d7319d28b5b
Brett Feddersen <bfeddrsn@illinois.edu>
2023-02-01 13:49:49 -0600

    Added signal for zero range data

--- src/Sensors/Lidar/Ouster/OusterModel_net.cpp
+++ src/Sensors/Lidar/Ouster/OusterModel_net.cpp
@@ -504,6 +504,12 @@ void cOusterModel_net::onNewData(uint16_t frameID, const cOusterLidarData& data)
         mFrameCounter = 3;
         emit updateLidarData();
     }
+
+    if (!data.rangeDataIsValid())
+    {
+        QString msg("Lidar range data is zero.");
+        emit statusMessage(msg);
+    }
 }
 
 void cOusterModel_net::retrieveConfigParam()
