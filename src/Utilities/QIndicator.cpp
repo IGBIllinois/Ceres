6a70240a8a457b7516341eeb8c3d664ee6127817
Brett Feddersen <bfeddrsn@illinois.edu>
2023-07-18 16:58:31 -0500

    Added a button type indicator

--- src/Utilities/QIndicator.cpp
+++ src/Utilities/QIndicator.cpp
@@ -1,18 +1,89 @@
 
 #include "QIndicator.hpp"
-#include <QPainter>
 
-QIndicator::QIndicator(const QString& text, QWidget* parent)
+#include <QApplication>
+#include <QPainter>
+#include <QStylePainter>
+#include <QStyleOption>
+
+
+/*
+extern bool qt_is_gui_used;
+
+Q_GUI_EXPORT int qt_defaultDpiX()
+{
+	if (QCoreApplication::instance()->testAttribute(Qt::AA_Use96Dpi))
+		return 96;
+
+	if (!qt_is_gui_used)
+		return 75;
+
+	if (const QScreen* screen = QGuiApplication::primaryScreen())
+		return qRound(screen->logicalDotsPerInchX());
+
+	//PI has not been initialised, or it is being initialised. Give a default dpi
+	return 100;
+}
+*/
+
+QT_BEGIN_NAMESPACE
+	
+Q_GUI_EXPORT int qt_defaultDpiX();
+
+QT_END_NAMESPACE
+
+namespace QStyleHelper
+{
+
+#ifdef Q_OS_DARWIN
+	static const qreal qstyleBaseDpi = 72;
+#else
+	static const qreal qstyleBaseDpi = 96;
+#endif
+
+	qreal dpi(const QStyleOption* option)
+	{
+#ifndef Q_OS_DARWIN
+		// Prioritize the application override, except for on macOS where
+		// we have historically not supported the AA_Use96Dpi flag.
+		if (QCoreApplication::testAttribute(Qt::AA_Use96Dpi))
+			return 96;
+#endif
+
+		// Expect that QStyleOption::QFontMetrics::QFont has the correct DPI set
+		if (option)
+			return option->fontMetrics.fontDpi();
+
+		// Fall back to historical Qt behavior: hardocded 72 DPI on mac,
+		// primary screen DPI on other platforms.
+#ifdef Q_OS_DARWIN
+		return qstyleBaseDpi;
+#else
+		return qt_defaultDpiX();
+#endif
+	}
+
+	qreal dpiScaled(qreal value, qreal dpi)
+	{
+		return value * dpi / qstyleBaseDpi;
+	}
+}
+
+
+/////////////////////////////////////////////////////////////////////
+// L E D   I N D I C A T O R
+/////////////////////////////////////////////////////////////////////
+QLedIndicator::QLedIndicator(const QString& text, QWidget* parent)
 :
-	QIndicator(parent)
+	QLedIndicator(parent)
 {
 	mText.setText(text);
 
 	auto text_size = mText.size();
-	setFixedSize(mLedSize + 10 + text_size.width(), mLedSize + 10);
+	setFixedSize(mLedSize + mMargin + text_size.width(), mLedSize + mMargin);
 }
 
-QIndicator::QIndicator(QWidget* parent)
+QLedIndicator::QLedIndicator(QWidget* parent)
 :
 	QWidget(parent), mLit(false)
 {
@@ -22,84 +93,304 @@ QIndicator::QIndicator(QWidget* parent)
 	mLedOffColor   = Qt::red;
 	mLedOffPattern = Qt::SolidPattern;
 
+	mMargin = 10;
 	mLedSize = 12;
-	setFixedSize(mLedSize + 10, mLedSize + 10);
+	setFixedSize(mLedSize + mMargin, mLedSize + mMargin);
 }
 
-QIndicator::~QIndicator()
+QLedIndicator::~QLedIndicator()
 {}
 
-void QIndicator::setText(const QString& text)
+void QLedIndicator::setText(const QString& text)
 {
 	mText.setText(text);
 
 	auto text_size = mText.size();
-	setFixedSize(mLedSize + 10 + text_size.width(), mLedSize + 10);
+	setFixedSize(mLedSize + mMargin + text_size.width(), mLedSize + mMargin);
 
 	update();
 }
 
-QString QIndicator::text() const
+QString QLedIndicator::text() const
 {
 	return mText.text();
 }
 
-void QIndicator::toggle()
+void QLedIndicator::toggle()
 {
 	mLit = !mLit;
 	update();
 }
 
-void QIndicator::setOnColor(QColor onColor)
+void QLedIndicator::setOnColor(QColor onColor)
 {
 	mLedOnColor = onColor;
 	update();
 }
 
-void QIndicator::setOnPattern(Qt::BrushStyle onPattern)
+void QLedIndicator::setOnPattern(Qt::BrushStyle onPattern)
 {
 	mLedOnPattern = onPattern;
 	update();
 }
 
-void QIndicator::setOffColor(QColor offColor)
+void QLedIndicator::setOffColor(QColor offColor)
 {
 	mLedOffColor = offColor;
 	update();
 }
 
-void QIndicator::setOffPattern(Qt::BrushStyle offPattern)
+void QLedIndicator::setOffPattern(Qt::BrushStyle offPattern)
 {
 	mLedOffPattern = offPattern;
 	update();
 }
 
-bool QIndicator::getState() const
+bool QLedIndicator::getState() const
 {
 	return mLit;
 }
 
-void QIndicator::setState(bool state)
+void QLedIndicator::setState(bool state)
 {
 	mLit = state;
 	update();
 }
 
-void QIndicator::setLedSize(int size)
+int  QLedIndicator::margin() const { return mMargin;  }
+
+void QLedIndicator::setMargin(int margin)
 {
-	mLedSize = size;
+	mMargin = margin < 0 ? 0 : margin;
+}
+
+void QLedIndicator::setLedSize(int size)
+{
+	mLedSize = size < 0 ? 0 : size;
 
 	auto text_size = mText.size();
-	setFixedSize(mLedSize + 10 + text_size.width(), mLedSize + 10);
+	setFixedSize(mLedSize + mMargin + text_size.width(), mLedSize + mMargin);
 
 	update();
 }
 
-void QIndicator::paintEvent(QPaintEvent* event)
+void QLedIndicator::paintEvent(QPaintEvent* event)
 {
 	QPainter p(this);
 	mLit ? p.setBrush(QBrush(mLedOnColor, mLedOnPattern)) : p.setBrush(QBrush(mLedOffColor, mLedOffPattern));
 	p.drawEllipse(0, 0, mLedSize, mLedSize);
 	p.drawStaticText(mLedSize + 5, 0, mText);
+}
+
+
+
+/////////////////////////////////////////////////////////////////////
+// B U T T O N   I N D I C A T O R
+/////////////////////////////////////////////////////////////////////
+QButtonIndicator::QButtonIndicator(const QString& text, QWidget* parent)
+	:
+	QButtonIndicator(parent)
+{
+	mText = text;
+}
+
+QButtonIndicator::QButtonIndicator(QWidget* parent)
+	:
+	QWidget(parent), mUnknownBrush(Qt::gray, Qt::SolidPattern),
+	mErrorBrush(Qt::red, Qt::SolidPattern), mWarningBrush(Qt::yellow, Qt::SolidPattern),
+	mAlertBrush(QColor(255, 191,0), Qt::SolidPattern), mInfoBrush(Qt::white, Qt::SolidPattern),
+	mOkBrush(Qt::green, Qt::SolidPattern), mActiveBrush(QColor(0, 179, 0), Qt::SolidPattern)
+{
+}
+
+QButtonIndicator::~QButtonIndicator()
+{}
+
+void QButtonIndicator::setText(const QString& text)
+{
+	mText = text;
+	repaint();
+}
+
+QString QButtonIndicator::text() const
+{
+	return mText;
+}
+
+QButtonIndicator::eState QButtonIndicator::getState() const
+{
+	return mState;
+}
+
+void QButtonIndicator::setState(eState state)
+{
+	mState = state;
+	repaint();
+}
+
+void QButtonIndicator::setState(eState state, const QString& text)
+{
+	mState = state;
+	mText = text;
+	repaint();
+}
+
+void QButtonIndicator::setStateColor(eState state, QColor color)
+{
+	switch (mState)
+	{
+	case UNKNOWN:
+		mUnknownBrush.setColor(color);
+		break;
+	case ERROR:
+		mErrorBrush.setColor(color);
+		break;
+	case WARNING:
+		mWarningBrush.setColor(color);
+		break;
+	case ALERT:
+		mAlertBrush.setColor(color);
+		break;
+	case INFO:
+		mInfoBrush.setColor(color);
+		break;
+	case OK:
+		mOkBrush.setColor(color);
+		break;
+	case ACTIVE:
+		mActiveBrush.setColor(color);
+		break;
+	}
+}
+
+void QButtonIndicator::setStatePattern(eState state, Qt::BrushStyle pattern)
+{
+	switch (mState)
+	{
+	case UNKNOWN:
+		mUnknownBrush.setStyle(pattern);
+		break;
+	case ERROR:
+		mErrorBrush.setStyle(pattern);
+		break;
+	case WARNING:
+		mWarningBrush.setStyle(pattern);
+		break;
+	case ALERT:
+		mAlertBrush.setStyle(pattern);
+		break;
+	case INFO:
+		mInfoBrush.setStyle(pattern);
+		break;
+	case OK:
+		mOkBrush.setStyle(pattern);
+		break;
+	case ACTIVE:
+		mActiveBrush.setStyle(pattern);
+		break;
+	}
+}
+
+QSize QButtonIndicator::minimumSizeHint() const
+{
+	return sizeHint();
+}
+
+QSize QButtonIndicator::sizeHint() const
+{
+	if (mSizeHint.isValid())
+		return mSizeHint;
+
+	ensurePolished();
+
+	int w = 0, h = 0;
+
+	QStyleOptionButton opt;
+	initStyleOption(&opt);
+
+	// calculate contents size...
+	QString s(mText);
+	bool empty = s.isEmpty();
+	if (empty)
+		s = QStringLiteral("XXXX");
+	QFontMetrics fm = fontMetrics();
+	QSize sz = fm.size(Qt::TextShowMnemonic, s);
+	if (!empty || !w)
+		w += sz.width();
+	if (!empty || !h)
+		h = qMax(h, sz.height());
+	opt.rect.setSize(QSize(w, h));
+
+	mSizeHint = (style()->sizeFromContents(QStyle::CT_PushButton, &opt, QSize(w, h), this).
+		expandedTo(QApplication::globalStrut()));
+
+	return mSizeHint;
+
+}
+
+void QButtonIndicator::initStyleOption(QStyleOptionButton* option) const
+{
+	if (!option)
+		return;
+
+	option->initFrom(this);
+	option->features = QStyleOptionButton::Flat;
+	option->state = QStyle::State_Enabled | QStyle::State_On;
+	option->text = mText;
+}
+
+void QButtonIndicator::paintEvent(QPaintEvent* event)
+{
+	QStyleOptionButton opt;
+	initStyleOption(&opt);
+
+	QStylePainter p(this);
+
+	// Save pen/brush
+	QPen oldPen = p.pen();
+	QBrush oldBrush = p.brush();
+
+	const qreal dpi = QStyleHelper::dpi(&opt);
+	const auto topLevelAdjustment = QStyleHelper::dpiScaled(0.5, dpi);
+	const auto bottomRightAdjustment = QStyleHelper::dpiScaled(-1, dpi);
+
+	// Set the background fill color based on the state
+	switch (mState)
+	{
+	case UNKNOWN:
+		p.setBrush(mUnknownBrush);
+		break;
+	case ERROR:
+		p.setBrush(mErrorBrush);
+		break;
+	case WARNING:
+		p.setBrush(mWarningBrush);
+		break;
+	case ALERT:
+		p.setBrush(mAlertBrush);
+		break;
+	case INFO:
+		p.setBrush(mInfoBrush);
+		break;
+	case OK:
+		p.setBrush(mOkBrush);
+		break;
+	case ACTIVE:
+		p.setBrush(mActiveBrush);
+		break;
+	}
+
+	p.setPen(QPen(Qt::black, 0));
+	p.drawRoundedRect(QRectF(opt.rect).adjusted(0, 0, -topLevelAdjustment, -topLevelAdjustment), mRadius, mRadius);
+
+	// Restore pen/brush
+	p.setPen(oldPen);
+	p.setBrush(oldBrush);
+
+	// Draw the text...
+	QRect textRect = opt.rect;
+	uint tf = Qt::AlignVCenter | Qt::AlignHCenter | Qt::TextHideMnemonic;
+
+	style()->drawItemText(&p, textRect, tf, opt.palette, true, mText, QPalette::ButtonText);
 }
 
--- src/Utilities/QIndicator.hpp
+++ src/Utilities/QIndicator.hpp
@@ -5,15 +5,29 @@
 #include <QStaticText>
 #include <QTextOption>
 
+// Qt Forward Declaration
+QT_BEGIN_NAMESPACE
+    class QStyleOptionButton;
+QT_END_NAMESPACE
 
-class QIndicator : public QWidget
+
+/**
+ * An LED based indicator
+ * 
+ * A two state indicator with text.  The indicator has the following layout:
+ * 
+ *      o  Text
+ * 
+ * The circle will show either the off color/pattern or the on color/pattern.
+ */
+class QLedIndicator : public QWidget
 {
     Q_OBJECT
 
 public:
-    QIndicator(QWidget* parent = nullptr);
-    QIndicator(const QString& text, QWidget* parent = nullptr);
-	virtual ~QIndicator();
+    QLedIndicator(QWidget* parent = nullptr);
+    QLedIndicator(const QString& text, QWidget* parent = nullptr);
+	virtual ~QLedIndicator();
 
 	void setText(const QString& text);
 	QString text() const;
@@ -29,6 +43,12 @@ public:
     void setOffColor(QColor offColor);
     void setOffPattern(Qt::BrushStyle offPattern);
 
+    int  margin() const;
+    void setMargin(int margin);
+
+    /*
+     * Sets the size of the LED circle.
+     */
     void setLedSize(int size);
 
 signals:
@@ -49,4 +69,79 @@ private:
     Qt::BrushStyle mLedOffPattern;
 
     int mLedSize;
+    int mMargin;
 };
+
+
+
+/**
+ * A button based indicator (looks similar to a push button w/o button action)
+ *
+ * A multi-state indicator with text.  The indicator has the following layout:
+ *
+ *      /--------\
+ *      |  Text  |
+ *      \--------/
+ *
+ * The default size is the same as a push button.
+ * Each state of the indicator can have its own color/pattern of the background.
+ * The defaults are:
+ *      UNKNOWN   - gray, solid
+ *      ERROR     - red, solid
+ *      WARNING   - yellow, solid
+ *      ALERT     - amber, solid
+ *      INFO      - white, solid
+ *      OK        - green, solid
+ *      ACTIVE    - dark-green, solid
+ */
+class QButtonIndicator : public QWidget
+{
+    Q_OBJECT
+
+public:
+    enum eState {UNKNOWN, ERROR, WARNING, ALERT, INFO, OK, ACTIVE};
+
+public:
+    QButtonIndicator(QWidget* parent = nullptr);
+    QButtonIndicator(const QString& text, QWidget* parent = nullptr);
+    virtual ~QButtonIndicator();
+
+    void setText(const QString& text);
+    QString text() const;
+
+    eState getState() const;
+    void setState(eState state);
+    void setState(eState state, const QString& text);
+
+    void setStateColor(eState state, QColor color);
+    void setStatePattern(eState state, Qt::BrushStyle pattern);
+
+    QSize minimumSizeHint() const override;
+    QSize sizeHint() const override;
+
+signals:
+    void stateChanged(eState state);
+
+protected:
+    void initStyleOption(QStyleOptionButton* option) const;
+    void paintEvent(QPaintEvent* event) override;
+
+private:
+    eState  mState = eState::UNKNOWN;
+    QString mText;
+
+    mutable QSize mSizeHint;
+
+    QBrush  mUnknownBrush;
+    QBrush  mErrorBrush;
+    QBrush  mWarningBrush;
+    QBrush  mAlertBrush;
+    QBrush  mInfoBrush;
+    QBrush  mOkBrush;
+    QBrush  mActiveBrush;
+
+    int mRadius = 5;
+};
+
+
+
