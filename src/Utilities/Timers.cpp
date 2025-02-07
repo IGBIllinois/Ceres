c46548981d138e65ea1fae32ebbda589fb729502
Brett Feddersen <bfeddrsn@illinois.edu>
2025-02-07 16:54:17 -0600

    Added a one shot timer

--- src/Utilities/Timers.cpp
+++ src/Utilities/Timers.cpp
@@ -6,6 +6,7 @@ using namespace std::chrono;
 cIntervalTimer::cIntervalTimer()
 	: cIntervalTimer(100)
 {
+	mRunning = false;
 }
 
 cIntervalTimer::cIntervalTimer(uint32_t interval_ms)
@@ -14,10 +15,20 @@ cIntervalTimer::cIntervalTimer(uint32_t interval_ms)
 	mInterval = std::chrono::milliseconds(interval_ms);
 }
 
+bool cIntervalTimer::is_running() const
+{
+	return mRunning;
+}
+
+bool cIntervalTimer::is_stopped() const
+{
+	return !mRunning;
+}
+
 uint32_t cIntervalTimer::interval_sec() const
 {
-	seconds ms = duration_cast<seconds>(mInterval);
-	return ms.count();
+	seconds secs = duration_cast<seconds>(mInterval);
+	return secs.count();
 }
 
 void cIntervalTimer::interval_sec(uint32_t interval_sec)
@@ -56,6 +67,72 @@ bool cIntervalTimer::elapsed()
 	if (diff > mInterval)
 	{
 		mStartTime = now;
+		return true;
+	}
+
+	return false;
+}
+
+
+
+cOneShotTimer::cOneShotTimer()
+{
+	mRunning = false;
+}
+
+bool cOneShotTimer::is_running() const
+{
+	return mRunning;
+}
+
+bool cOneShotTimer::is_stopped() const
+{
+	return !mRunning;
+}
+
+uint32_t cOneShotTimer::time_sec() const
+{
+	seconds secs = duration_cast<seconds>(mInterval);
+	return secs.count();
+}
+
+void cOneShotTimer::time_sec(uint32_t interval_sec)
+{
+	mInterval = std::chrono::seconds(interval_sec);
+}
+
+uint32_t cOneShotTimer::time_ms() const
+{
+	milliseconds ms = duration_cast<milliseconds>(mInterval);
+	return ms.count();
+}
+
+void cOneShotTimer::time_ms(uint32_t interval_ms)
+{
+	mInterval = std::chrono::milliseconds(interval_ms);
+}
+
+void cOneShotTimer::start()
+{
+	mRunning = true;
+	mStartTime = std::chrono::high_resolution_clock::now();
+}
+
+/*
+void cOneShotTimer::stop()
+{
+	mRunning = false;
+}
+*/
+
+bool cOneShotTimer::elapsed()
+{
+	if (!mRunning) return false;
+	auto now = std::chrono::high_resolution_clock::now();
+
+	auto diff = now - mStartTime;
+	if (diff > mInterval)
+	{
 		return true;
 	}
 
--- src/Utilities/Timers.hpp
+++ src/Utilities/Timers.hpp
@@ -10,6 +10,9 @@ public:
 	cIntervalTimer(uint32_t interval_ms);
 	~cIntervalTimer() = default;
 
+	bool is_running() const;
+	bool is_stopped() const;
+
 	uint32_t interval_sec() const;
 	void interval_sec(uint32_t interval_sec);
 
@@ -24,6 +27,32 @@ public:
 
 private:
 	bool mRunning = true;
+	std::chrono::high_resolution_clock::time_point mStartTime;
+	std::chrono::nanoseconds mInterval;
+};
+
+
+class cOneShotTimer
+{
+public:
+	cOneShotTimer();
+	~cOneShotTimer() = default;
+
+	bool is_running() const;
+	bool is_stopped() const;
+
+	uint32_t time_sec() const;
+	void time_sec(uint32_t interval_sec);
+
+	uint32_t time_ms() const;
+	void time_ms(uint32_t interval_ms);
+
+	void start();
+
+	bool elapsed();
+
+private:
+	bool mRunning = false;
 	std::chrono::high_resolution_clock::time_point mStartTime;
 	std::chrono::nanoseconds mInterval;
 };
