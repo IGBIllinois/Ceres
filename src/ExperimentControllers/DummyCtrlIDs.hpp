4d5fb5a465ceb83d7486821f68dc07be15310f59
Brett Feddersen <brett.feddersen@outlook.com>
2025-02-08 11:08:54 -0600

    Initial commit: switching to use separate id files

---
+++ src/ExperimentControllers/DummyCtrlIDs.hpp
@@ -0,0 +1,6 @@
+
+#pragma once
+
+
+static const char* dummy_id = "dummy";
+
---
+++ src/ExperimentControllers/Spidercam/SpidercamCtrlIDs.hpp
@@ -0,0 +1,8 @@
+
+#pragma once
+
+#include "../ExperimentCtrlFactory.hpp"
+
+static const char* spidercam_id = "spidercam";
+
+sExperimentControllerWidgets create_spidercam_controller(bool no_visualization = false);
