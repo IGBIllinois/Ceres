439e4cbf15da849e7c78cbc95ea74606500924f0
Brett Feddersen <bfeddrsn@illinois.edu>
2025-08-02 13:19:14 -0500

    Initial commit

---
+++ src/apps/ExperimentPlanner/RappAerialModel.cpp
@@ -0,0 +1,136 @@
+
+#include "RappAerialModel.hpp"
+#include "RappFieldBoundary.hpp"
+
+#include <QString>
+
+
+namespace
+{
+    cRappAerialModel* pDataModel = nullptr;
+}
+
+cRappAerialModel* cRappAerialModel::get()
+{
+    return pDataModel;
+}
+
+cRappAerialModel::cRappAerialModel()
+{
+    pDataModel = this;
+}
+
+cRappAerialModel::~cRappAerialModel()
+{
+}
+
+//-----------------------------------------------------------------------------
+std::size_t cRappAerialModel::numOfTriangles() const
+{
+    return mAerialMesh.size();
+}
+
+std::size_t cRappAerialModel::numOfAerialPoints() const
+{
+    return mAerialPoints.size();
+}
+
+void cRappAerialModel::clearAerialPoints()
+{
+    mAerialPoints.clear();
+}
+
+void cRappAerialModel::clearAerialMesh()
+{
+    mAerialMesh.clear();
+    mQuadTree.reset();
+}
+
+void cRappAerialModel::addAerialPoint(const rfm::planePoint_t& gps_point)
+{
+    if (!rfb::withinBoundary(gps_point))
+        return;
+
+    auto point = rfb::toRappCoordinates(gps_point);
+
+    mAerialPoints.push_back(point);
+}
+
+void cRappAerialModel::addAerialPoint(const rfm::rappPoint_t& rapp_point)
+{
+    if (!rfb::withinBoundary(rapp_point))
+        return;
+
+    mAerialPoints.push_back(rapp_point);
+}
+
+void cRappAerialModel::addAerialPoints(const std::vector<rfm::planePoint_t>& gps_points)
+{
+    for (const auto& gps_point : gps_points)
+    {
+        addAerialPoint(rfb::toRappCoordinates(gps_point));
+    }
+}
+
+void cRappAerialModel::addAerialPoints(const std::vector<rfm::rappPoint_t>& rapp_points)
+{
+    for (const auto& rapp_point : rapp_points)
+    {
+        addAerialPoint(rapp_point);
+    }
+}
+
+void cRappAerialModel::addMeshPoint(const rfm::rappPoint_t& p1, const rfm::rappPoint_t& p2, const rfm::rappPoint_t& p3)
+{
+    mAerialMesh.emplace_back(p1, p2, p3);
+}
+
+void cRappAerialModel::addMeshData(const std::vector<cRappTriangle>& mesh)
+{
+    if (!mQuadTree)
+    {
+        mQuadTree = std::make_unique<cQuadTree>(rfb::minX_mm(), rfb::maxX_mm(), rfb::minY_mm(), rfb::maxY_mm());
+    }
+
+    for (auto& triangle : mesh)
+    {
+        mAerialMesh.emplace_back(triangle);
+        mQuadTree->addMeshData(triangle);
+    }
+}
+
+const std::vector<rfm::rappPoint_t>& cRappAerialModel::getAerialPoints() const
+{
+    return mAerialPoints;
+}
+
+double cRappAerialModel::getMeshHeight_mm(std::int32_t x_mm, std::int32_t y_mm) const
+{
+    if (mQuadTree)
+    {
+        auto h = mQuadTree->height(x_mm, y_mm);
+
+        if (h != rfm::INVALID_HEIGHT)
+            return h;
+    }
+
+    for (auto triangle : mAerialMesh)
+    {
+        if (triangle.withinTriangle(x_mm, y_mm))
+        {
+            if (mQuadTree)
+            {
+                auto* pNode = mQuadTree->getNode(x_mm, y_mm);
+                if (pNode)
+                    pNode->addMeshData(triangle);
+            }
+
+            return triangle.height(x_mm, y_mm);
+        }
+    }
+
+    return rfm::INVALID_HEIGHT;
+}
+
+
+
