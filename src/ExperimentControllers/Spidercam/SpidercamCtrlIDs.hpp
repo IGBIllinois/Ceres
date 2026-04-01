90cac2b9f27504a0ad6c97f4ad445b90598d16f6
Brett Feddersen <bfeddrsn@illinois.edu>
2026-04-01 10:51:30 -0500

    Removed unused include file

--- src/ExperimentControllers/Spidercam/SpidercamCtrlIDs.hpp
+++ src/ExperimentControllers/Spidercam/SpidercamCtrlIDs.hpp
@@ -1,8 +1,5 @@
 
 #pragma once
 
-#include "../ExperimentCtrlFactory.hpp"
-
 static const char* spidercam_id = "spidercam";
 
-sExperimentControllerWidgets create_spidercam_controller(bool no_visualization = false);
