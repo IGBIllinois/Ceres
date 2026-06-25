ebead6313cf2497a2026de69713bb5aa2843d8df
Brett Feddersen <bfeddrsn@illinois.edu>
2026-06-25 17:24:22 -0500

    Initial commit

---
+++ src/ExperimentStateMachine/ExperimentVariableNames.hpp
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include <string_view>
+
+namespace nExperimentVariables
+{
+	static constexpr std::string_view SENSOR_OFFSET_mm = "sensor_offset_mm";
+
+	static constexpr std::string_view CANOPY_HEIGHT_mm = "canopy_height_mm";
+
+	static constexpr std::string_view REFERENCE_HEIGHT_mm = "reference_height_mm";
+}
+
